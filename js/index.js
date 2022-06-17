dc3cd882fd6455362596b806f2b6a979872a80b7
igor <igor.alves@dbserver.com.br>
2022-06-17 15:22:33 -0300

    fazendo a tela principal do front-end

--- js/index.js
+++ js/index.js
@@ -1,3 +1,45 @@
 
+function alertaU(){
+    window.location.href="./UnidadesSaudeMenu.html";
+}
+
+function alertaA(){
+    alert("Para os atendimentos");
+}
+
+function alertaE(){
+    alert("Para as etapas de atendimentos");
+}
+
+function alertaT(){
+    alert("Para os testes dos atendimentos");
+}
+
+
 const botaoU = document.querySelector(".botaoU");
 
+botaoU.addEventListener(
+    "click",
+    alertaU
+)
+
+const botaoA = document.querySelector(".botaoA");
+
+botaoA.addEventListener(
+    "click",
+    alertaA
+)
+
+const botaoE = document.querySelector(".botaoE");
+
+botaoE.addEventListener(
+    "click",
+    alertaE
+)
+
+const botaoT = document.querySelector(".botaoT");
+
+botaoT.addEventListener(
+    "click",
+    alertaT
+)
