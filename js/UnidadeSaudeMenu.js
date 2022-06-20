6b53250435b2cbfb90db204f2cc855f484b48b62
igor <igor.alves@dbserver.com.br>
2022-06-20 15:39:56 -0300

    fazendo a tela que por enquanto lista unidades de saúde do front-end

---
+++ js/UnidadeSaudeMenu.js
@@ -0,0 +1,36 @@
+
+var unidades = document.querySelector('[data-u]');
+
+function loadUnidadesDeSaude(result){
+    for(let u of result){
+        const linhaNova = document.createElement('tr');
+        const conteudo = `
+        <td>
+        <ul>
+        <li>
+            <a>ID: ${u.id}</a>
+            <h1></h1>
+            <a>Numero pacientes: ${u.numeroPacientes}</a>
+            <h1></h1>
+            <a>Data: ${u.data}</a>
+        </li>
+    </ul>
+    </td>
+        `
+        linhaNova.innerHTML = conteudo;
+        unidades.appendChild(linhaNova);
+    }
+}
+
+const http = new XMLHttpRequest();
+
+http.onreadystatechange = function() {
+    if (http.readyState == XMLHttpRequest.DONE) {
+        var result = JSON.parse(http.response);
+        loadUnidadesDeSaude(result);
+    }
+}
+
+http.open("GET","http://localhost:8080/unidades/todas");
+http.send();
+
