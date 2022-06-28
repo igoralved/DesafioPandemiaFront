667d7ade5d8165da7a3edcaec131595509d618f1
igoralved <87991535+igoralved@users.noreply.github.com>
2022-06-28 18:12:30 +0000

    Add files via upload

---
+++ js/menu/EtapasAtendimento.js
@@ -0,0 +1,39 @@
+
+var etapas = document.querySelector('[data-e]');
+
+function loadEtapas(result){
+    for(let e of result){
+        const linhaNova = document.createElement('tr');
+        
+        const conteudo = `
+        <td>
+        <ul>
+        <h1></h1>
+        <li>
+            <a>ID: ${e.id}</a>
+            <h1></h1>
+            <a>Numero: ${e.numero}</a>
+            <h1></h1>
+            <a>Descricao: ${e.descricao}</a>
+            <h1></h1>
+        </li>
+        <h1></h1>
+    </ul>
+    </td>
+        `
+        linhaNova.innerHTML = conteudo;
+        etapas.appendChild(linhaNova);
+    }
+}
+
+const http = new XMLHttpRequest();
+
+http.onreadystatechange = function() {
+    if (http.readyState == XMLHttpRequest.DONE) {
+        var result = JSON.parse(http.response);
+        loadEtapas(result);
+    }
+}
+
+http.open("GET","http://localhost:8080/atendimentos/"+localStorage.getItem("idA")+"/etapas");
+http.send();
